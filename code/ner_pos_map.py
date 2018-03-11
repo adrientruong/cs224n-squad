3abb57bdca57762b9e98e450ce8760d8a2aa4b31
Adrien Truong <adrien.truong@me.com>
2018-03-11 04:11:10 -0700

    Add NER POS map

---
+++ code/ner_pos_map.py
@@ -0,0 +1,48 @@
+def NER_map():
+	labels = [
+		'',
+		'PERSON',
+		'NORP',
+		'FACILITY',
+		'ORG',
+		'GPE',
+		'LOC',
+		'PRODUCT',
+		'EVENT',
+		'WORK_OF_ART',
+		'LAW',
+		'LANGUAGE',
+		'DATE',
+		'TIME',
+		'PERCENT',
+		'MONEY',
+		'QUANTITY',
+		'ORDINAL',
+		'CARDINAL'
+	]
+	return {label: i for i, label in enumerate(labels)}
+
+def POS_map():
+	labels = [
+		'ADJ',
+		'ADP',
+		'ADV',
+		'AUX',
+		'CONJ',
+		'CCONJ',
+		'DET',
+		'INTJ',
+		'NOUN',
+		'NUM',
+		'PART',
+		'PRON',
+		'PROPN',
+		'PUNCT',
+		'SCONJ',
+		'SYM',
+		'VERB',
+		'X',
+		'SPACE'
+	]
+
+	return {label: i for i, label in enumerate(labels)}
